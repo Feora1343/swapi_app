896e23930357b72d88b86875329d70392484169d
Feora1343 <renee.vorbeck@gmail.com>
2018-03-03 19:53:55 -0600

    theme change

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,28 @@
 /** ---------- ANGULAR MODULE ---------- **/
-const myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngAria', 'ngMessages']);
-
+const myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngAria', 'ngMessages'])
+  /* ---------- CHANGE THEME AS NEEDED FOR YOUR PROJECT ---------- */
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('blue-grey')
+    .accentPalette('blue-grey');
+    
 /** ---------- MODULE CONFIGURATION ---------- **/
 myApp.config(($routeProvider, $mdThemingProvider) => {
   console.log('config loaded');
 
-  /* ---------- DEFINE CLIENT SIDE ROUTES ---------- */
-  // CHANGE TEMPLATEURL AND CONTROLLER AS NEEDED FOR YOUR PROJECT
-  $routeProvider
-  .when('/search', {
+    /* ---------- DEFINE CLIENT SIDE ROUTES ---------- */
+    // CHANGE TEMPLATEURL AND CONTROLLER AS NEEDED FOR YOUR PROJECT
+    $routeProvider
+    .when('/search', {
       templateUrl: '/views/search.html',
       controller: 'SearchController as sc'
-  })
-  .when('/favorites', {
+    })
+    .when('/favorites', {
       templateUrl: '/views/favorites.html',
       controller: 'FavoritesController as fc'
-  })
-  .otherwise(
-      { redirectTo: '/search' }
-  );
-  /* ---------- CHANGE THEME AS NEEDED FOR YOUR PROJECT ---------- */
-  $mdThemingProvider.theme('default')
-    .primaryPalette('blue-grey')
-    .accentPalette('blue-grey');
+    })
+    .otherwise(
+    { redirectTo: '/search' }
+    );
+    })
 });
